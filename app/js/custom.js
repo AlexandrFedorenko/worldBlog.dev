7004fdddf0da4ca07a995755c9490a1ac20ca8cf
Alexandr <alexandrfedorenkoooo@gmail.com>
2018-01-26 17:59:20 +0200

    add desktop nav

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,7 +1,16 @@
 $(function() {
     // no dragstart img
     $("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
 });
+
+//search show
+// $(".search_btn").click(function (e) {
+//     e.preventDefault();
+//     if( $(".search_inp").hasClass('search_inp--show')){
+//         $(".search_btn").submit();
+//     }else{
+//         $(".search_inp").toggleClass('search_inp--show');
+//     }
+// });
 
 
