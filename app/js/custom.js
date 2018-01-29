afefcd28d5ac77b21edc2ddf99ccc1ddd1775a03
Alexandr <alexandrfedorenkoooo@gmail.com>
2018-01-29 17:55:58 +0200

    add 60% index page

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,16 +1,96 @@
-$(function() {
+$(function () {
     // no dragstart img
-    $("img, a").on("dragstart", function(event) { event.preventDefault(); });
+    $("img, a").on("dragstart", function (event) {
+        event.preventDefault();
+    });
 });
 
-//search show
-// $(".search_btn").click(function (e) {
-//     e.preventDefault();
-//     if( $(".search_inp").hasClass('search_inp--show')){
-//         $(".search_btn").submit();
-//     }else{
-//         $(".search_inp").toggleClass('search_inp--show');
-//     }
-// });
+//burger mobile menu
+var clickDelay = 500,
+    clickDelayTimer = null;
 
+$('.burger-click-region').on('click', function () {
 
+    if (clickDelayTimer === null) {
+
+        var $burger = $(this);
+        $burger.toggleClass('active');
+        $burger.parent().toggleClass('is-open');
+
+        if (!$burger.hasClass('active')) {
+            $burger.addClass('closing');
+        }
+
+        clickDelayTimer = setTimeout(function () {
+            $burger.removeClass('closing');
+            clearTimeout(clickDelayTimer);
+            clickDelayTimer = null;
+        }, clickDelay);
+    }
+
+    $(".col-flex").slideToggle();
+    $(".burger-menu-piece").toggleClass('white');
+
+});
+
+//main-slider
+$('.main-slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    dots: false,
+    arrows: true,
+    prevArrow: '<div class="prev arr-left"></div>',
+    nextArrow: '<div class="next arr-right"></div>',
+    responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 2,
+                infinite: true
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 2,
+                arrows: false,
+                autoplay:true,
+                infinite: true
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 2,
+                arrows: false,
+                autoplay:true,
+                infinite: true
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                arrows: false,
+                autoplay:true,
+                infinite: true
+            }
+        }
+
+    ]
+});
+
+//open posts mobil
+
+$('.posts').click(openPostMob);
+
+function openPostMob() {
+    $(this).find('.fa-down').toggleClass('fa-act');
+    $(this).find('.wrap-post-ul').slideToggle();
+    $(this).find('.wrap-country').slideToggle();
+}
